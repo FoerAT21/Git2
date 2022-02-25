8be3a0007bde86a1ad5924b3fbb0eecf93105577
FOERSTAT21 <FoerstAt21@gcc.edui>
2022-02-25 14:21:08 -0500

    Get ricked

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("I am clearly cooler than you guys :)");
+        System.out.println("Stevie is clearly less cool than all of us combined.");
     }
 }
-    
