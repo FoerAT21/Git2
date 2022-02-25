b9429656a82f54156e92225b8e742aab1bd3f0bc
KESTERHOLTEM21 <kesterholtem21@gcc.edu>
2022-02-25 14:24:12 -0500

    Hewwo

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("I am clearly cooler than you guys :)");
+        System.out.println("Andrew is stinky");
     }
 }
     
